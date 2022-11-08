5b0f3bb073bef2b281ab866cc1bfb84a4a8dbf11
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2022-11-08 17:09:57 +0100

    add default propagation

--- tinygraph/message_passing.py
+++ tinygraph/message_passing.py
@@ -1,5 +1,5 @@
 from tinygrad.tensor import Tensor
-from utils import
+from .utils import average
 
 class MessagePassing:
     """Implements a basic message passing layer. 
@@ -11,8 +11,21 @@ class MessagePassing:
         if not message_func: message_func = self._identity
         if not propagation_func: propagation_func = self._default_propagation
 
-    def _default_propagation(x: Tensor, msg: Tensor):
-        
+    def _default_propagation(self, x: Tensor, msg: Tensor):
+        """Internal function that implements default aggregation which takes the mean of x and the messages.
+        The Tensor gets reshaped internally.
+
+        Args:
+            x (Tensor): (len, ) shaped
+            msg (Tensor): (msg, ) shaped
+
+        Returns:
+            Tensor: (len, ) shaped
+        """
+        x = x.reshape((1, x.shape[0]))
+        msg = msg.reshape((1, msg.shape[0]))
+        x_dash = x.cat(msg).mean(axis = 0)
+        return x_dash
 
 
     def _identity(x: Tensor) -> Tensor:
