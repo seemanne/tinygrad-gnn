9320aec03236370551286fa43d261c7728b394f5
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2022-11-08 16:36:10 +0100

    finalize average

--- tinygraph/utils/average.py
+++ tinygraph/utils/average.py
@@ -2,13 +2,21 @@ from tinygrad.tensor import Tensor
 import numpy as np
 
 def average(A: Tensor) -> Tensor:
-    len = A.shape[1]
-    x = Tensor(np.ones((len, 1)))
-    y = A.matmul(x)
+    """Averages the matrix column wise.
+    Meant to be used as an aggregation function during aggregation steps.
+
+    Args:
+        A (Tensor): 2d Tensor with each row containing features from a different vertex.
+
+    Returns:
+        Tensor: Column-wise average of A
+    """
+    len = A.shape[0]
+    x = Tensor(np.ones((len, 1)), requires_grad=True)
+    y = A.transpose().matmul(x).div(len)
     return y
 
-A = Tensor([[2, 1],[1, 0]], requires_grad= True)
-y = average(A)
 
-a = y.reshape(y.shape[0]).backward()
-f = 'clown'
+
+#A = Tensor([[2, 1],[1, 0], [0,1]], requires_grad= True)
+#y = average(A)
