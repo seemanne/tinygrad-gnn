b210409d83f6ba3a33631f5c129a4bfd533e09f7
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2022-11-09 10:14:49 +0100

    change to tensor_manipulation stacking

--- tinygraph/message_passing.py
+++ tinygraph/message_passing.py
@@ -1,15 +1,19 @@
 from tinygrad.tensor import Tensor
 from .utils import average
+from .utils import tensor_manipulation
 
 class MessagePassing:
     """Implements a basic message passing layer. 
     """
-    def __init__(self, x, adjacency, aggr, message_func = None, propagation_func = None) -> None:
-        self.x = x
-        self.adjacency = adjacency
-        self.aggr = aggr
-        if not message_func: message_func = self._identity
-        if not propagation_func: propagation_func = self._default_propagation
+    def __init__(self, aggr = None, message_func = None, propagation_func = None) -> None:
+        if not aggr: self.aggr = self._default_aggregation
+        else: self.aggr = aggr
+
+        if not message_func: self.message_func = self._identity
+        else: self.message_func = message_func
+
+        if not propagation_func: self.propagation_func = self._default_propagation
+        else: self.propagation_func = propagation_func
 
     def _default_propagation(self, x: Tensor, msg: Tensor):
         """Internal function that implements default aggregation which takes the mean of x and the messages.
@@ -22,11 +26,11 @@ class MessagePassing:
         Returns:
             Tensor: (len, ) shaped
         """
-        x = x.reshape((1, x.shape[0]))
-        msg = msg.reshape((1, msg.shape[0]))
-        x_dash = x.cat(msg).mean(axis = 0)
+        x_dash = tensor_manipulation.stack_rows([x, msg]).mean(axis = 0)
         return x_dash
 
+    def _default_aggregation(self):
+        return
 
     def _identity(x: Tensor) -> Tensor:
         """Internal function that returns the input tensor. 
